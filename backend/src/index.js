c63efa1d875c61870031ebd4929a80adc874667d
chiru417 <chiruchintha9@gmail.com>
2026-09-13 11:22:51 +0530

    add clerk to react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
 
 const app = express();
-
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 connectDB();
 app.listen(3000, () => {
   console.log("you are listening to the port 3000");
