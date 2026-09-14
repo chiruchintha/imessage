63722a4be2817110ad6fdf5c263ef35ef37b46c7
chiru417 <chiruchintha9@gmail.com>
2026-09-14 08:27:52 +0530

    add docker file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,23 @@ import "dotenv/config";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 connectDB();
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 app.listen(3000, () => {
   console.log("you are listening to the port 3000");
 });
