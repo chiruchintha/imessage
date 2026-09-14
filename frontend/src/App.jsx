cf34822764ceb65fb667f8d5d1df3dfe6663abc1
chiru417 <chiruchintha9@gmail.com>
2026-09-14 19:43:41 +0530

    testing clerk key in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 function App() {
   return (
     <>
+      {console.log("Clerk key:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY)}
       <header>
         <h1>chat app</h1>
         <Show when="signed-out">
